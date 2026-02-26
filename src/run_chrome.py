588cfaed44a1ee2a3306054ffb01e86011c3431f
kevinHJ99 <kevinhj1099@gmail.com>
2026-02-26 16:58:50 -0500

    fix run_chrome

--- src/run_chrome.py
+++ src/run_chrome.py
@@ -1,8 +1,34 @@
 import subprocess
+import platform
+import logging
 import time
+import shutil
+import winreg
 import socket
 
 class ChromeManager:
+        
+    @staticmethod
+    def find_chrome():
+        system = platform.system()
+
+        path = shutil.which("chrome") or \
+        shutil.which("google-chrome") or \
+        shutil.which("chrome.exe")
+
+        if path:
+            return path
+
+        if system == "Windows":
+            path = r"SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\chrome.exe"
+            for root in [winreg.HKEY_LOCAL_MACHINE, winreg.HKEY_CURRENT_USER]:
+                try:
+                    with winreg.OpenKey(root, path) as key:
+                        return winreg.QueryValue(key, None)
+                except FileNotFoundError:
+                    continue
+            return None
+    
     @staticmethod
     def is_port_open(port):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -10,9 +36,18 @@ class ChromeManager:
 
     @staticmethod
     def ensure_chrome():
+        chrome_path = None
+
+        find_chrome_path = ChromeManager.find_chrome()
+
+        if not find_chrome_path:
+            logging.error("Google Chrome path could not be found.")
+        else:
+            chrome_path = find_chrome_path
+
         if not ChromeManager.is_port_open(9222):
             subprocess.Popen([
-                r"C:\Program Files\Google\Chrome\Application\chrome.exe",
+                chrome_path,
                 "--remote-debugging-port=9222",
                 "--user-data-dir=C:\\chrome-automation",
                 "--no-first-run",
